04f0d7c2d0d3ef4fae7e0ee2dd8a88b7a51753fa
david singh rolson <davidrolson@ninjacart.com>
2025-12-13 00:01:28 +0530

    printwidth fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,11 @@ import ReactQueryProvider from "@/lib/queryClient/ReactQueryProvider";
 import "./globals.css";
 import { ThemeProvider } from "./theme-provider";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
