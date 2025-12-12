e193a22139d1ef09c8d1b89971f562dea4f6fdd8
david singh rolson <davidrolson@ninjacart.com>
2025-12-12 22:46:45 +0530

    fix

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,24 +1,20 @@
 "use client";
 
-
 import RegisterForm from "@/components/auth/RegisterForm";
 import Link from "next/link";
 export default function LoginForm() {
-  
-
   return (
     <div className="w-[380px] bg-white dark:bg-[#1a1a22] rounded-2xl p-8 shadow-xl">
-        <RegisterForm/>
-        <p className="mt-6 text-center text-sm text-gray-600 dark:text-gray-400">
-  Already have an account?{" "}
-  <Link
-    href="/auth/login"
-    className="text-indigo-600 dark:text-indigo-400 font-medium hover:underline"
-  >
-    Login
-  </Link>
-</p>
-</div>
-    
+      <RegisterForm />
+      <p className="mt-6 text-center text-sm text-gray-600 dark:text-gray-400">
+        Already have an account?{" "}
+        <Link
+          href="/auth/login"
+          className="text-indigo-600 dark:text-indigo-400 font-medium hover:underline"
+        >
+          Login
+        </Link>
+      </p>
+    </div>
   );
 }
